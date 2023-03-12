25473495b69075f65c4caf17c62046a63de636c0
Spylon <100944659+SewerynRoznowski@users.noreply.github.com>
2023-03-12 12:44:35 +0100

    Markers initial

---
+++ cfgExtendedEventHandlers.hpp
@@ -0,0 +1,14 @@
+class Extended_PreInit_EventHandlers {
+    class pxg_markers_init {
+        init = "call pxg_markers_fnc_preInit;";
+    };
+};
+
+
+
+class Extended_PostInit_EventHandlers 
+{
+    class pxg_markers_init {
+        init = "call pxg_markers_fnc_initMarkers;";
+    };
+};
---
+++ config.cpp
@@ -0,0 +1,17 @@
+class CfgFunctions
+{   
+    #include <markers\cfgFunctions.hpp>
+};
+
+
+class CfgRemoteExec
+{
+    #include <CfgRemoteExec.hpp>
+};
+
+#include <macros.hpp>
+
+#include <cfgExtendedEventHandlers.hpp>
+
+#include <markers\config.cpp>
+

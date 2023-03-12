c2cbf5e26d1dc7a868c19d8f2bc6546ad23fca15
Spylon <100944659+SewerynRoznowski@users.noreply.github.com>
2023-03-12 17:26:25 +0100

    Respawn Menu touchup

--- respawn/dialogs/respawnDialog.hpp
+++ respawn/dialogs/respawnDialog.hpp
@@ -10,8 +10,8 @@ class dialog_respawn
 
 			x = 0.38 * safezoneW + safezoneX;
 			y = 0.26 * safezoneH + safezoneY;
-			w = 0.33 * safezoneW;
-			h = 0.27 * safezoneH;
+			w = 0.18 * safezoneW;
+			h = 0.32 * safezoneH;
 		};
 		
 		class RscStructuredText_1: PxgGuiRscStructuredText
@@ -20,7 +20,7 @@ class dialog_respawn
 			text = "Respawn menu"; //--- ToDo: Localize;
 			x = 0.38 * safezoneW + safezoneX;
 			y = 0.238 * safezoneH + safezoneY;
-			w = 0.33 * safezoneW;
+			w = 0.18 * safezoneW;
 			h = 0.022 * safezoneH;
 			sizeEx = 1 * GUI_GRID_H * GUI_GRID_H;
 		};
@@ -30,32 +30,10 @@ class dialog_respawn
 
 			text = "";
 			x = 0.39 * safezoneW + safezoneX;
-			y = 0.48 * safezoneH + safezoneY;
-			w = 0.18 * safezoneW;
+			y = 0.53 * safezoneH + safezoneY;
+			w = 0.16 * safezoneW;
 			h = 0.04 * safezoneH;
 		};
-/*		class respawn_add_wave_button: PxgGuiRscButton
-		{
-			idc = 471600;
-			action = "[-1] call pxg_respawn_fnc_addRespawnWave; closeDialog 2;";
-			text = "Add wave"; //--- ToDo: Localize;
-			
-			x = 0.50 * safezoneW + safezoneX;
-			y = 0.53 * safezoneH + safezoneY;
-			w = 0.06 * safezoneW;
-			h = 0.02 * safezoneH;
-		};
-		class respawn_remove_wave_button: PxgGuiRscButton
-		{
-			idc = 471601;
-			action = "[1] call pxg_respawn_fnc_addRespawnWave; closeDialog 2;";
-			text = "Remove wave"; //--- ToDo: Localize;
-			
-			x = 0.44 * safezoneW + safezoneX;
-			y = 0.53 * safezoneH + safezoneY;
-			w = 0.06 * safezoneW;
-			h = 0.02 * safezoneH;
-		};*/
 		class respawn_trigger_wave_button: PxgGuiRscButton
 		{
 			idc = 471602;
@@ -63,8 +41,8 @@ class dialog_respawn
 
 			text = "Trigger wave"; //--- ToDo: Localize;
 			
-			x = 0.56 * safezoneW + safezoneX;
-			y = 0.53 * safezoneH + safezoneY;
+			x = 0.44 * safezoneW + safezoneX;
+			y = 0.58 * safezoneH + safezoneY;
 			w = 0.06 * safezoneW;
 			h = 0.02 * safezoneH;
 		};
@@ -76,7 +54,7 @@ class dialog_respawn
 			text = "Close";
 
 			x = 0.38 * safezoneW + safezoneX;
-			y = 0.53 * safezoneH + safezoneY;
+			y = 0.58 * safezoneH + safezoneY;
 			w = 0.06 * safezoneW;
 			h = 0.02 * safezoneH;
 		};
@@ -84,9 +62,9 @@ class dialog_respawn
 		{
 			idc = 471604;
 
-			x = 0.58 * safezoneW + safezoneX;
+			x = 0.39 * safezoneW + safezoneX;
 			y = 0.32 * safezoneH + safezoneY;
-			w = 0.12 * safezoneW;
+			w = 0.16 * safezoneW;
 			h = 0.20 * safezoneH;
 
 		};
@@ -97,8 +75,8 @@ class dialog_respawn
 
 			text = "Respawn One";
 
-			x = 0.65 * safezoneW + safezoneX;
-			y = 0.53 * safezoneH + safezoneY;
+			x = 0.50 * safezoneW + safezoneX;
+			y = 0.58 * safezoneH + safezoneY;
 			w = 0.06 * safezoneW;
 			h = 0.02 * safezoneH;
 		};
@@ -107,7 +85,7 @@ class dialog_respawn
 			idc = 471606; 
 
 			text = "Dead Players:";
-			x = 0.58 * safezoneW + safezoneX;
+			x = 0.39 * safezoneW + safezoneX;
 			y = 0.27 * safezoneH + safezoneY;
 			w = 0.05 * safezoneW;
 			h = 0.04 * safezoneH;

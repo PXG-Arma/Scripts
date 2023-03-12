be14f1908bc26484b6b539bcf447ed7c2b336b26
Spylon <100944659+SewerynRoznowski@users.noreply.github.com>
2023-03-12 16:10:34 +0100

    Hotfix
    Markers did not initiate properly because of typo.

--- cfgExtendedEventHandlers.hpp
+++ cfgExtendedEventHandlers.hpp
@@ -10,9 +10,9 @@ class Extended_PreInit_EventHandlers {
 class Extended_PostInit_EventHandlers 
 {
     class pxg_respawn_init {
-        init = "call pxg_respawn_fnc_initRespawn;";
+        init = "call pxg_respawn_fnc_postInit;";
     };
     class pxg_markers_init {
-        init = "call pxg_markers_fnc_initRespawn;";
+        init = "call pxg_markers_fnc_postInit;";
     };
 };
